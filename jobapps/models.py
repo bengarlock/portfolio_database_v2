3f3be14c1c130b2209d2ddb3db223d41ce88c485
github username <github email address>
2021-04-01 20:47:39 -0400

    updated jobapps model to match old database

--- jobapps/models.py
+++ jobapps/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
 class Jobapp(models.Model):
-    name = models.CharField(max_length=100)
-    body = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100)
+    job_title = models.CharField(max_length=200, blank=True)
+    url = models.CharField(max_length=1000)
     owner = models.ForeignKey(User, related_name="jobapps", on_delete=models.CASCADE, null=True)
+    technologies = ArrayField(models.CharField(max_length=255), default=list, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    status = models.TextField(default="Applied", max_length=50, blank=True)
+    contact = models.TextField(default="Hiring Manager", blank=True)
