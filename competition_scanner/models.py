092d6eb073accb1cfaaa1a0af1f44c541ce889ef
github username <github email address>
2021-04-18 20:44:30 -0400

    added url to resy model

--- competition_scanner/models.py
+++ competition_scanner/models.py
@@ -6,7 +6,7 @@ class ResyRestaurant(models.Model):
     restaurant_name = models.CharField(max_length=3000, blank=True)
     resy_id = models.CharField(max_length=1000, blank=True)
     neighborhood = models.CharField(max_length=3000, blank=True)
-    url = models.CharField(max_length=3000, blank=True)
+    url = models.CharField(max_length=3000, blank=True, null=True)
 
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True)
