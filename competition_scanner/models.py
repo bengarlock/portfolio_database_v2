7782f7bb0a5a96e23370cd5928fb411bb511b40c
github username <github email address>
2021-07-12 20:28:43 -0400

    sync commit

--- competition_scanner/models.py
+++ competition_scanner/models.py
@@ -13,10 +13,12 @@ class ResyRestaurant(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True)
 
+
 class ResyTotalCount(models.Model):
     total_count = models.IntegerField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True)
+
 
 class YelpRestaurant(models.Model):
     restaurant_name = models.CharField(max_length=3000, blank=True)
@@ -26,6 +28,7 @@ class YelpRestaurant(models.Model):
     active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True)
+
 
 class YelpTotals(models.Model):
     total_count = models.IntegerField(blank=True)
