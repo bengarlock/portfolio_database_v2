81efb665e7071189c38a0757a15f84e5c4023e97
github username <github email address>
2021-07-19 22:31:40 -0400

    troubleshooting migration error

--- health_tracker/models.py
+++ health_tracker/models.py
@@ -6,4 +6,4 @@ class Player(models.Model):
     name = models.CharField(max_length=3000, default="", null=True, blank=True)
     status = models.IntegerField(default=0)
     goal = models.IntegerField(default=0)
-    photo = models.ImageField(null=True, blank=True)
+    #photo = models.ImageField(null=True, blank=True)
