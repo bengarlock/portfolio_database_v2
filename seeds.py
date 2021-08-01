6365f6e2538b01474f2f5270e094951217df5de6
github username <github email address>
2021-08-01 15:16:41 -0400

    updated seed file

--- seeds.py
+++ seeds.py
@@ -5,7 +5,7 @@ from faker import Faker
 import random
 
 #url = "http://127.0.0.1:8000/"
-url = "http://127.0.0.1:8000/api/v1/tablehost/"
+url = "https://bengarlock.com/api/v1/tablehost/"
 today = date.today()
 books = []
 
